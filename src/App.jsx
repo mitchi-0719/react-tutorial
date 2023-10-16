c53158cbf94a9b2b285a12183e8f70bb4b58d8d9
saku <pg.saku0719@gmail.com>
2023-10-16 19:44:49 +0900

    style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-// import "./styles/global.css"
 import "./styles/style.css"
 
 export default function App() {
